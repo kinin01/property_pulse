a4a40d73c82d626ce7738576a682c0fd499199a1
kinin01 <kiningumbuvi@gmail.com>
2024-05-01 23:02:16 +0100

    cleaning up

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -20,6 +20,9 @@ import Spinner from '@/components/spinner';
 import Image from 'next/image';
 import PropertyDetails from '@/components/PropertyDetails';
 import PropertyImages from '@/components/PropertyImages';
+import BookmarkButton from '@/components/BookmarkButton';
+import ShareButtons from "@/components/ShareButtons";
+import PropertyContactForm from "@/components/PropertyContactForm";
 
 
 
@@ -78,65 +81,15 @@ const PropertyPage = () => {
                 <PropertyDetails property={property} />
                 
                 <aside className='space-y-4'>
-                  <div>
-                    <button className='bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
-                      <FaBookmark className='mr-2' />
-                      Bookmark Property
-                    </button>
-                  </div>
-                  <div>
-                    <button className='bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
-                      <FaShare className='mr-2' /> Share Property
-                    </button>
-                  </div>
+                  
+                  <BookmarkButton property={property} />
+    
+                  <ShareButtons property={property} />
 
-                  {/* <!-- Contact Form --> */}
-                  <div className='bg-white p-6 rounded-lg shadow-md'>
-                    <h3 className='text-xl font-bold mb-6'>
-                      Contact Property Manager
-                    </h3>
-                    <form
-                      action='mailto:support@traversymedia.com'
-                      method='post'
-                      encType='text/plain'
-                    >
-                      <div className='mb-4'>
-                        <label
-                          className='block text-gray-700 text-sm font-bold mb-2'
-                          htmlFor='email'
-                        >
-                          Email:
-                        </label>
-                        <input
-                          className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                          id='email'
-                          type='email'
-                          placeholder='Enter your email'
-                        />
-                      </div>
-                      <div className='mb-4'>
-                        <label
-                          className='block text-gray-700 text-sm font-bold mb-2'
-                          htmlFor='message'
-                        >
-                          Message:
-                        </label>
-                        <textarea
-                          className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 h-44 focus:outline-none focus:shadow-outline'
-                          id='message'
-                          placeholder='Enter your message'
-                        ></textarea>
-                      </div>
-                      <div>
-                        <button
-                          className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center'
-                          type='submit'
-                        >
-                          <FaPaperPlane className='mr-2' /> Send Message
-                        </button>
-                      </div>
-                    </form>
-                  </div>
+                  <PropertyContactForm property={property} />
+                  
+
+                 
                 </aside>
               </div>
             </div>
